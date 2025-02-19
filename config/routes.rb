a4f5862f7475406b88ca43be22abdc0149be4727
NkanyisoMz <104601010+NkanyisoMz@users.noreply.github.com>
2025-02-19 23:50:32 +0200

    Completed Forms Project

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :users, only: [ :new, :create ]
+  root "users#new"  # Change "users#new" to whatever your homepage controller#action is.
+
+  resources :users, only: [ :new, :create, :edit, :update ]
 end
